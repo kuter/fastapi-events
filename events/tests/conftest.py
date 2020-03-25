1c894f7e51aa42c2409377b82ead0925e7521a51
kuter <contact@devktr.pl>
2020-03-25 07:38:54 +0100

    :art: change imports

--- events/tests/conftest.py
+++ events/tests/conftest.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from alembic.command import downgrade, upgrade
-from alembic.config import Config as AlembicConfig
+from alembic.config import Config
 
 TEST_DB_URL = "sqlite:///./test.db"
 ALEMBIC_INI = Path(__file__).parents[2] / "alembic.ini"
@@ -16,7 +16,7 @@ def db(request):
     engine = create_engine(TEST_DB_URL, echo=True)
     Session = sessionmaker(bind=engine)
 
-    alembic_config = AlembicConfig(ALEMBIC_INI)
+    alembic_config = Config(ALEMBIC_INI)
     alembic_config.set_main_option("sqlalchemy.url", TEST_DB_URL)
     upgrade(alembic_config, "head")
 
